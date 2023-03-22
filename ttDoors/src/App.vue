00fa525a7f7d32f0c0f60aeb578dea33186d9ad0
maxim <maxim.mehanikov@codex-soft.com>
2023-03-22 18:14:22 +0300

    feat: init scene with models

---
+++ ttDoors/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Scene from './components/Scene.vue'
+</script>
+
+<template>
+  <main>
+    <Scene />
+  </main>
+</template>
---
+++ ttDoors/src/components/Scene.vue
@@ -0,0 +1,13 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+import useScene from './Scene.js';
+
+export default {
+  setup() {
+    return useScene();
+  },
+}
+</script>
